cfd2126a40b36699903eeba7a3ad7d7fe7b9a4b6
Steven Zhu <stevenzhu03@gmail.com>
2020-02-25 14:42:33 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -12,4 +12,78 @@ document.addEventListener("DOMContentLoaded", () => {
       toyForm.style.display = "none";
     }
   });
+
+  fetch('http://localhost:3000/toys')
+  .then(resp => resp.json())
+  .then(toys => toys.forEach(addAToy))
+
+  
+  document.addEventListener('submit', event => {
+    event.preventDefault()
+    const theToyForm = document.querySelector(".add-toy-form");
+    let toyName = event.target.name.value
+    let toyImage = event.target.image.value
+    
+    let newToy = {
+      name: toyName,
+      image: toyImage,
+      likes: 0
+    }
+
+    fetch('http://localhost:3000/toys',{
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify(newToy)
+    })
+    .then(resp => resp.json())
+    .then(toy => {
+      
+      addAToy(toy) 
+      theToyForm.reset()
+      addToy = !addToy
+      toyForm.style.display = "none"
+
+    })
+
+  })
+  
+  document.addEventListener('click', event =>{
+    
+    if (event.target.className === 'like-btn'){
+      let likes = parseInt(event.target.previousElementSibling.innerText)
+      likes++
+      
+      fetch(`http://localhost:3000/toys/${event.target.parentNode.dataset.id}`,{
+        method: 'PATCH',
+        headers: {
+          "content-type": 'application/json',
+          'accept': 'application/json'
+        },
+        body: JSON.stringify({"likes": likes})
+      })
+      .then(resp => resp.json())
+      .then(response => {
+        event.target.previousElementSibling.innerText = `${response.likes} Likes`
+      })
+    }
+  })
+  
+  function addAToy(toy){
+    let toyCollection = document.getElementById('toy-collection')
+    let li = document.createElement('li')
+    li.className = "card"
+    li.dataset.id = toy.id
+  
+    li.innerHTML = `
+      <h2>${toy.name}</h2>
+      <img src=${toy.image} class="toy-avatar" />
+      <p>${toy.likes} Likes </p>
+      <button class="like-btn">Like <3</button>
+    `
+  
+    toyCollection.append(li)
+  }
 });
